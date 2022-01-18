767903d297cbbac0b0967b06cecd963e67b0b80c
Neerja <neerja.jha@publicissapient.com>
2022-01-18 22:59:15 +1100

    Adding mono example

---
+++ ReactiveProgrammingSamples/src/main/java/MonoJust.java
@@ -0,0 +1,16 @@
+import reactor.core.publisher.Mono;
+
+public class MonoJust {
+
+    public static void main(String[] args) {
+
+        // publisher
+        Mono<Integer> mono = Mono.just(1);
+        System.out.println(mono);
+
+        mono.subscribe(i -> System.out.println("Received : " + i));
+
+    }
+
+
+}
